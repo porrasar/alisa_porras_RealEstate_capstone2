fc68f8b078393397ff03b1391d7a8eea683e8361
Alisa Porras <aljuporras@gmail.com>
2023-06-26 14:09:16 -0400

    - took error out

--- src/main/java/com/perscholas/RealEstate/controllers/MainController.java
+++ src/main/java/com/perscholas/RealEstate/controllers/MainController.java
@@ -16,7 +16,7 @@ public class MainController
    @GetMapping("/")
    public String rootHomePage()
     {
-        return "html/home_page_general"   //ERRORrrr
+        return "html/home_page_general";   //ERRORrrr
     }
 
 //NEED TO ADD IF STATEMENTS
