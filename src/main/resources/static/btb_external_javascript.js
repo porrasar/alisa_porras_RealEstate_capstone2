3f1d79bcb4b18e6810cdccdef6964178faaeab1e
Alisa Porras <aljuporras@gmail.com>
2023-07-15 13:33:48 -0400

    - still working on slide show

--- src/main/resources/static/btb_external_javascript.js
+++ src/main/resources/static/btb_external_javascript.js
@@ -41,30 +41,47 @@ document.getElementById('dateDisplay').innerHTML = Date();
 }
 
 // ----------------------------- Carousel ----------------------------
-/*
-let slideIndex = 1;
-showSlides(slideIndex);
-*/
+////LOGIC - HAVE TO CLICK ON DOTS TO MAKE IT GO
+
+//let slideIndex = 1;
+//showSlides(slideIndex);
+//
+//// Next/previous controls
+//function plusSlides(n) {
+//  showSlides(slideIndex += n);
+//}
+//
+//// Thumbnail image controls
+//function currentSlide(n)
+// {
+//  showSlides(slideIndex = n);
+//}
+//
+//*
+//function showSlides(n)
+//{
+//  let i;
+//  let slides = document.getElementsByClassName("mySlides");
+//  let dots = document.getElementsByClassName("dot");
+//  if (n > slides.length) {slideIndex = 1}
+//  if (n < 1) {slideIndex = slides.length}
+//  for (i = 0; i < slides.length; i++) {
+//    slides[i].style.display = "none";
+//  }
+//  for (i = 0; i < dots.length; i++) {
+//    dots[i].className = dots[i].className.replace(" active", "");
+//  }
+//  slides[slideIndex-1].style.display = "block";
+//  dots[slideIndex-1].className += " active";
+//}
 
 
-// --- MAKES IT AUTOMATIC
+
+//LOGIC - MAKES THE SLIDESHOW AUTOMATIC
+
 let slideIndex = 0;
 showSlides();
-// --- end - MAKES IT AUTOMATIC
 
-
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-// --- MAKES IT AUTOMATIC
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
@@ -74,28 +91,17 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 2000); // Change image every 4 seconds
 }
-// --- end - MAKES IT AUTOMATIC
 
-
-
-/*
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
-*/
+
+// Thumbnail image controls
+function currentSlide(n)
+ {
+  showSlides(slideIndex = n);
+}
 // ----------------------------- Carousel End-----------------------------
-
