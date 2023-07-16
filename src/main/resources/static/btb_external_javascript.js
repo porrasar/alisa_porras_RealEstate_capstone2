8c7fb58af78765ff48d19b68ee45229ac654efca
Alisa Porras <aljuporras@gmail.com>
2023-07-16 15:16:01 -0400

    - with west theme song again

--- src/main/resources/static/btb_external_javascript.js
+++ src/main/resources/static/btb_external_javascript.js
@@ -40,43 +40,8 @@ function dateDisplayFunction()                            //used in general home
 document.getElementById('dateDisplay').innerHTML = Date();
 }
 
-// ----------------------------- Carousel ----------------------------
-////LOGIC - HAVE TO CLICK ON DOTS TO MAKE IT GO
 
-//let slideIndex = 1;
-//showSlides(slideIndex);
-//
-//// Next/previous controls
-//function plusSlides(n) {
-//  showSlides(slideIndex += n);
-//}
-//
-//// Thumbnail image controls
-//function currentSlide(n)
-// {
-//  showSlides(slideIndex = n);
-//}
-//
-//*
-//function showSlides(n)
-//{
-//  let i;
-//  let slides = document.getElementsByClassName("mySlides");
-//  let dots = document.getElementsByClassName("dot");
-//  if (n > slides.length) {slideIndex = 1}
-//  if (n < 1) {slideIndex = slides.length}
-//  for (i = 0; i < slides.length; i++) {
-//    slides[i].style.display = "none";
-//  }
-//  for (i = 0; i < dots.length; i++) {
-//    dots[i].className = dots[i].className.replace(" active", "");
-//  }
-//  slides[slideIndex-1].style.display = "block";
-//  dots[slideIndex-1].className += " active";
-//}
-
-
-
+<!-- ------------------------------------------------------------------------------------- -->
 //LOGIC - MAKES THE SLIDESHOW AUTOMATIC
 
 let slideIndex = 0;
